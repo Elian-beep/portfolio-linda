de72f1f7c67271ee11075e9375c8931eb55797dc
Elian-beep <elian.19batista@gmail.com>
2026-02-25 21:06:29 -0400

    feat: [Home] base struct in section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,30 @@
+import styles from "./Home.module.scss";
+import lindaImage from "@/assets/linda-logo-01.png";
+
 export const Home = () => {
   return (
-    <div id="home" style={{ padding: "2rem", paddingTop: "6rem" }}>
-      <h1>Sobre mim</h1>
-    </div>
+    <section id="home" className={styles.home}>
+      <div className={styles.imageArea}>
+        <img
+          src={lindaImage}
+          alt="Linda Souza"
+          className={styles.responsiveImage}
+        />
+      </div>
+
+      <div className={styles.descArea}>
+        <h1>Oi, eu sou a Linda Souza!</h1>
+
+        <p>
+          Product Designer apaixonada por tornar experiências digitais mais
+          acessíveis, simples e funcionais
+        </p>
+
+        <div className={styles.buttonArea}>
+          <button>Ver projetos →</button>
+          <button>Ver currículo PDF →</button>
+        </div>
+      </div>
+    </section>
   );
 };
