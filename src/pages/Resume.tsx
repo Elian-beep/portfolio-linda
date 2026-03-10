f90a77d06e406f9633e1df99410e6ee60457fa36
Elian-beep <elian.19batista@gmail.com>
2026-03-10 16:21:35 -0400

    fix: [CONTACT] Full widht in buttons

--- src/pages/Resume.tsx
+++
@@ -1,7 +0,0 @@
-export const Resume = () => {
-  return (
-    <div id="resume" style={{ padding: "2rem", paddingTop: "6rem" }}>
-      <h1>Currículo PDF</h1>
-    </div>
-  );
-};
--- src/pages/Services.tsx
+++
@@ -1,7 +0,0 @@
-export const Services = () => {
-  return (
-    <div id="services" style={{ padding: "2rem", paddingTop: "6rem" }}>
-      <h1>Diferencial e interesse</h1>
-    </div>
-  );
-};
